8fc6634172b634b8c1d59d55e2b56563c136195a
WNguyen14 <wnguyen1@ufl.edu>
2023-03-28 13:57:23 -0400

    initial website setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import React, { HtmlHTMLAttributes } from "react";
+const inter = Inter({ subsets: ["latin"] });
+const handleNavClick = (event: React.MouseEvent<HTMLElement>) => {
+  console.log(event);
+};
 export default function Home() {
   return (
     <>
@@ -14,110 +17,63 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
+
+      <header>
+        <div className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <a
+            href="/"
+            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+          >
+            <span className="fs-4"> DataViz</span>
+          </a>
+
+          <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
             <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+              className="me-3 py-2 link-body-emphasis text-decoration-none"
+              href="#"
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
+              Home
             </a>
+            <a
+              className="me-3 py-2 link-body-emphasis text-decoration-none"
+              href="#"
+            >
+              Data
+            </a>
+            <a
+              className="me-3 py-2 link-body-emphasis text-decoration-none"
+              href="#"
+            >
+              References
+            </a>
+          </nav>
+        </div>
+      </header>
+
+      <main className={styles.main}>
+        <section className="py-10 text-center container">
+          <div className="row py-lg-10">
+            <div className="col-lg-6 col-md-8 mx-auto">
+              <h1 className="fw-light">Welcome to DataViz</h1>
+              <p className="lead text-body-secondary">
+                Visualize the financial data through the lens of global events
+              </p>
+              <p>
+                <Link href="/posts/first-post" passHref>
+                  <button
+                    className="btn btn-primary my-2"
+                    onClick={(e) => {
+                      handleNavClick(e);
+                    }}
+                  >
+                    Get Started
+                  </button>
+                </Link>
+              </p>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        </section>
       </main>
     </>
-  )
+  );
 }
